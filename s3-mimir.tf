bec64ce6c691c9f713b31935c588063092101b82
maksym.onyshchenko <magzim@shalb.com>
2022-07-12 18:38:35 -0700

    feat: mimir production-grade

--- s3-mimir.tf
+++ s3-mimir.tf
@@ -1,3 +1,84 @@
+# This S3 storage is for Grafana Mimir Cluster
+
+
+resource "aws_s3_bucket" "k8s_bucket" {
+
+  bucket = "${var.tags.project}-${var.tags.environment}-mimir"
+  # tags = var.tags
+  # depends_on = [null_resource.s3_destroy]
+}
+
+
+
+resource "aws_iam_role" "mimr_cluster_minio" {
+  name               = "mimir-cluster-minio"
+  assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Federated": "${module.eks.oidc_provider_arn}"
+            },
+            "Action": "sts:AssumeRoleWithWebIdentity"
+        }
+    ]
+}
+POLICY
+
+  inline_policy {
+    name = "some_inline_policy"
+
+    # todo narrow down permissions to "reource ArnEquals" https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.4/deploy/installation/
+    policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "VisualEditor0",
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutAccountPublicAccessBlock",
+        "s3:GetAccountPublicAccessBlock",
+        "s3:ListAllMyBuckets",
+        "s3:ListJobs",
+        "s3:CreateJob",
+        "s3:HeadBucket",
+        "s3:ListBucket"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "VisualEditor1",
+      "Effect": "Allow",
+      "Action": "s3:*",
+      "Resource": [
+        "arn:aws:s3:::${var.tags.project}-${var.tags.environment}-mimir",
+        "arn:aws:s3:::${var.tags.project}-${var.tags.environment}-mimir/*"
+      ]
+    }
+  ]
+}
+POLICY
+
+  }
+}
+
+
+
+
+
+
+# resource "null_resource" "s3_destroy" {
+#   provisioner "local-exec" {
+#     # Unfortuenetelly can not use variables here or references to other resources
+#     command = "aws s3 rm s3://magz8s-stage --recursive"
+#     interpreter = ["bash", "-c"]
+#     when = "destroy"
+#   }
+# }
+
 
 # resource "aws_iam_user" "cluster_user" {
 #   name = "cluster-user"
@@ -5,21 +86,6 @@
 #   # tags = var.tags
 # }
 
-
-# resource "aws_s3_bucket" "k8s_bucket" {
-#     bucket = "${var.tags.project}-${var.tags.environment}"
-#     # tags = var.tags
-#     depends_on = [null_resource.s3_destroy]
-# }
-
-# # resource "null_resource" "s3_destroy" {
-# #   provisioner "local-exec" {
-# #     # Unfortuenetelly can not use variables here or references to other resources
-# #     command = "aws s3 rm s3://magz8s-stage --recursive"
-# #     interpreter = ["bash", "-c"]
-# #     when = "destroy"
-# #   }
-# # }
 
 
 
