3b5442b1cc4da596c9e7a2a9aaecc31f0b8a676a
maksym.onyshchenko <devops@maxim.run>
2022-07-19 16:34:28 -0700

    fix: hook and or error handling

--- terraspace/app/stacks/main/config/hooks/terraform.rb
+++ terraspace/app/stacks/main/config/hooks/terraform.rb
@@ -10,7 +10,7 @@ before("destroy",
   label: "Pruning loadbalancers created by kubernetes ingress controller",
   execute: "echo 'app/stacks/main/config/hooks/terraform.rb: test stack before hook for terraform destroy';
   terraform output eks_vpc.arn ;
-  aws elbv2 delete-load-balancer --load-balancer-arn $(terraform output eks_vpc.arn) || echo '\u001b[33m  Did not find ingress resources to prune. \u001b[0m' && echo '\u001b[32m  Pruned ingress load balancers  \u001b[33m'
+  aws elbv2 delete-load-balancer --load-balancer-arn $(terraform output eks_vpc.arn) && echo '\u001b[32m  Pruned ingress load balancers  \u001b[33m  || echo '\u001b[33m  Did not find ingress resources to prune. \u001b[0m' '
   ",
 )
 
