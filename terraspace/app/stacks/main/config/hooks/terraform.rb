f83b058672faa8714949e38b64b6a1a9a77cdace
maksym.onyshchenko <devops@maxim.run>
2022-07-20 17:47:20 -0700

    fix: terraspace destroy hook

--- terraspace/app/stacks/main/config/hooks/terraform.rb
+++ terraspace/app/stacks/main/config/hooks/terraform.rb
@@ -12,7 +12,7 @@ before("destroy",
   execute: "echo 'app/stacks/main/config/hooks/terraform.rb: test stack before hook for terraform destroy';
   test $(terraform output -json  eks_vpc )  || { echo '\u001b[33m  Unable to get eks_vpc output. \u001b[0m' ;  exit 1 }
   VPC_ID=$(terraform output -json  eks_vpc   | jq -r '.vpc_id' )
-  aws elbv2 delete-load-balancer --load-balancer-arn $(aws elbv2  describe-load-balancers --output json  | jq -r --arg VPC_ID $VPC_ID '.LoadBalancers[] | select(.VpcId==$VPC_ID) | .LoadBalancerArn ') && echo '\u001b[32m  Pruned ingress load balancers  \u001b[33m  || echo '\u001b[33m  Did not find ingress resources to prune. \u001b[0m' '
+  aws elbv2 delete-load-balancer --load-balancer-arn $(aws elbv2  describe-load-balancers --output json  | jq -r --arg VPC_ID $VPC_ID '.LoadBalancers[] | select(.VpcId==$VPC_ID) | .LoadBalancerArn ') && echo '\u001b[32m  Pruned ingress load balancers  \u001b[33m'  || echo '\u001b[33m  Did not find ingress resources to prune. \u001b[0m' 
   ",
   exit_on_fail: false,
 )
