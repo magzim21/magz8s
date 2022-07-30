2fc8a4e83de1553392fdcce6c984767443326058
maksym.onyshchenko <devops@maxim.run>
2022-07-30 15:58:57 -0700

    feat: new cluster - new yaml variables

--- terraspace/app/stacks/main/argocd.tf
+++ terraspace/app/stacks/main/argocd.tf
@@ -1,8 +1,8 @@
 resource "null_resource" "kubeconfig" {
   # todo: maybe remove this trigger and simplify script.
-  triggers = {
-    always_run = "${timestamp()}"
-  }
+  # triggers = {
+  #   always_run = "${timestamp()}"
+  # }
   provisioner "local-exec" {
 
     command = <<SCRIPT
@@ -33,6 +33,9 @@ SCRIPT
 
 
 resource "local_file" "external_dns" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
   content = templatefile("${path.module}/../../../../../../argo-projects-templates/addons/external-dns.yaml.tftpl", {
     "role-arn" : aws_iam_role.external_dns_controller.arn
   })
@@ -40,6 +43,9 @@ resource "local_file" "external_dns" {
   depends_on = [null_resource.kubeconfig, aws_iam_role.external_dns_controller]
 }
 resource "local_file" "cluster_autoscaler" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
   content = templatefile("${path.module}/../../../../../../argo-projects-templates/addons/cluster-autoscaler.yaml.tftpl", {
     "clusterName" : "${var.tags.project}-${var.tags.environment}",
     "awsRegion" : data.aws_region.current.id,
@@ -50,6 +56,9 @@ resource "local_file" "cluster_autoscaler" {
   depends_on = [null_resource.kubeconfig, aws_iam_role.cluster_autoscaler]
 }
 resource "local_file" "aws_efs_csi_driver" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
   content    = templatefile("${path.module}/../../../../../../argo-projects-templates/addons/aws-efs-csi-driver.yaml.tftpl", {
     aws_image_registry = local.aws_image_registry
     efs_id = module.efs.id
@@ -59,6 +68,9 @@ resource "local_file" "aws_efs_csi_driver" {
   depends_on = [null_resource.kubeconfig, module.efs]
 }
 resource "local_file" "ingress_controller" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
   content = templatefile("${path.module}/../../../../../../argo-projects-templates/addons/ingress-controller.yaml.tftpl", {
     "clusterName" : "${var.tags.project}-${var.tags.environment}",
     "role-arn" : aws_iam_role.ingress_controller.arn
@@ -67,16 +79,25 @@ resource "local_file" "ingress_controller" {
   depends_on = [null_resource.kubeconfig, aws_iam_role.ingress_controller]
 }
 # resource "local_file" "grafana" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
 #   content    = templatefile("${path.module}/../../../../../../argo-projects-templates/monitoring/grafana.yaml.tftpl", {})
 #   filename   = "${path.module}/../../../../../../argo-projects/monitoring/grafana.yaml"
 #   depends_on = [null_resource.kubeconfig]
 # }
 resource "local_file" "prometheus_stack" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
   content    = templatefile("${path.module}/../../../../../../argo-projects-templates/monitoring/prometheus-stack.yaml.tftpl", {})
   filename   = "${path.module}/../../../../../../argo-projects/monitoring/prometheus-stack.yaml"
   depends_on = [null_resource.kubeconfig]
 }
 resource "local_file" "grafana_mimir_custom" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
   content = templatefile("${path.module}/../../../../../../argo-projects-templates/monitoring/values/grafana-mimir-custom.yaml.tftpl", {
     "s3_bucket_admin" : aws_s3_bucket.metrics_admin.id,
     "s3_bucket_ruler" : aws_s3_bucket.metrics_ruler.id,
@@ -87,6 +108,9 @@ resource "local_file" "grafana_mimir_custom" {
   depends_on = [null_resource.kubeconfig]
 }
 resource "local_file" "grafana_mimir" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
   content    = templatefile("${path.module}/../../../../../../argo-projects-templates/monitoring/grafana-mimir.yaml.tftpl", {
     "repo_owner" : var.repo_owner,
     "repo_name" : var.repo_name,
@@ -96,16 +120,25 @@ resource "local_file" "grafana_mimir" {
   depends_on = [null_resource.kubeconfig]
 }
 resource "local_file" "argo_monitoring_project" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
   content    = templatefile("${path.module}/../../../../../../argo-projects-templates/monitoring/argo-monitoring-project.yaml.tftpl", {})
   filename   = "${path.module}/../../../../../../argo-projects/monitoring/argo-monitoring-project.yaml"
   depends_on = [null_resource.kubeconfig]
 }
 resource "local_file" "loki" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
   content    = templatefile("${path.module}/../../../../../../argo-projects-templates/monitoring/loki.yaml.tftpl", {})
   filename   = "${path.module}/../../../../../../argo-projects/monitoring/loki.yaml"
   depends_on = [null_resource.kubeconfig]
 }
 resource "local_file" "game_2048" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
   content = templatefile("${path.module}/../../../../../../argo-projects-templates/apps/game-2048.yaml.tftpl", {
     "repo_owner" : var.repo_owner,
     "repo_name" : var.repo_name,
@@ -117,6 +150,9 @@ resource "local_file" "game_2048" {
 
 }
 resource "local_file" "root_application" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
   content    = templatefile("${path.module}/../../../../../../root-application.yaml.tftpl", {
     "repo_owner" : var.repo_owner,
     "repo_name" : var.repo_name
@@ -127,6 +163,9 @@ resource "local_file" "root_application" {
 }
 
 resource "null_resource" "push_changes" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
   provisioner "local-exec" {
     # Todo: move git commnds to the 
     command = <<SCRIPT
