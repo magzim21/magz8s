1b4dd25ffcbe543fadef4cd57d3d05d900c1d961
maksym.onyshchenko <devops@maxim.run>
2022-07-29 21:02:15 -0700

    feat: new cluster - new yaml variables

--- terraspace/app/stacks/main/argocd.tf
+++ terraspace/app/stacks/main/argocd.tf
@@ -53,7 +53,8 @@ resource "null_resource" "gitops_branch" {
       existed_in_local=$(git branch --list $branch)
 
       if [[ -z $existed_in_local ]]; then
-          git checkout $branch
+          # git checkout $branch
+          echo Branch $branch already exists
       else
           git checkout -b $branch
       fi
