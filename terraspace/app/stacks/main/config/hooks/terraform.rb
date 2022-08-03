f70751753e45e5a5d7c2cf1e9271b072e5c3970d
maksym.onyshchenko <devops@maxim.run>
2022-08-02 22:11:30 -0700

    fix: terraspace hook command group

--- terraspace/app/stacks/main/config/hooks/terraform.rb
+++ terraspace/app/stacks/main/config/hooks/terraform.rb
@@ -10,7 +10,7 @@ before("destroy",
   label: "Pruning loadbalancers created by kubernetes ingress controller",
   # TODO more precise filter , not just vpc id. Seems that terraform output does not work here. Try  terraspace  output main     
   execute: "echo 'app/stacks/main/config/hooks/terraform.rb: test stack before hook for terraform destroy';
-  terraspace output main -json  eks_vpc  || { echo '\u001b[33m  Unable to get eks_vpc output. \u001b[0m' ;  exit 1 }
+  terraspace output main -json  eks_vpc  || { echo '\u001b[33m  Unable to get eks_vpc output. \u001b[0m' ;  exit 1 ; }
   VPC_ID=$(terraspace output main -json  eks_vpc   | jq -r '.vpc_id' )
   aws elbv2 delete-load-balancer --load-balancer-arn $(aws elbv2  describe-load-balancers --output json  | jq -r --arg VPC_ID $VPC_ID '.LoadBalancers[] | select(.VpcId==$VPC_ID) | .LoadBalancerArn ') && echo '\u001b[32m  Pruned ingress load balancers  \u001b[33m'  || echo '\u001b[33m  Did not find ingress resources to prune. \u001b[0m' 
   ",
