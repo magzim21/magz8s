c63d0adda39cf973d2653818f6316e0ec97312a8
maksym.onyshchenko <devops@maxim.run>
2022-08-03 18:15:28 -0700

    fix: prune SG

--- terraspace/app/stacks/main/config/hooks/terraform.rb
+++ terraspace/app/stacks/main/config/hooks/terraform.rb
@@ -12,7 +12,21 @@ before("destroy",
   execute: "echo 'app/stacks/main/config/hooks/terraform.rb: test stack before hook for terraform destroy';
   terraform output  -json  eks_vpc  || { echo '\u001b[33m  Unable to get eks_vpc output. \u001b[0m' ;  exit 1 ; }
   VPC_ID=$(terraform output  -json  eks_vpc   | jq -r '.vpc_id' )
-  aws elbv2 delete-load-balancer --load-balancer-arn $(aws elbv2  describe-load-balancers --output json  | jq -r --arg VPC_ID $VPC_ID '.LoadBalancers[] | select(.VpcId==$VPC_ID) | .LoadBalancerArn ') && echo '\u001b[32m  Pruned ingress load balancers  \u001b[33m'  || echo '\u001b[33m  Did not find ingress resources to prune. \u001b[0m' 
+  LBs_PRUNE=$(aws elbv2  describe-load-balancers --output json  | jq -r --arg VPC_ID $VPC_ID '.LoadBalancers[] | select(.VpcId==$VPC_ID) | .LoadBalancerArn ')
+  for LB in $LBs_PRUNE ; do
+    aws elbv2 delete-load-balancer --load-balancer-arn $LB  && echo '\u001b[32m  Pruned ingress load balancers  \u001b[33m'  || echo '\u001b[33m  Did not find ingress resources to prune. \u001b[0m' 
+  done
+  
+  CLUSTER_ID=$(terraform output  -json  eks   | jq -r '.cluster_id')
+  LB_ARN=$(
+    aws elbv2 describe-tags --resource-arns $(aws elbv2  describe-load-balancers --output json   | jq -r '.LoadBalancers[] | .LoadBalancerArn')  --output json  |
+      jq -r --arg  CLUSTER_ID $CLUSTER_ID  '.TagDescriptions[] |
+        select(
+            (.Tags |  index({\"Key\":\"elbv2.k8s.aws/cluster\",\"Value\":$CLUSTER_ID}) ) and 
+            (.Tags | index({\"Key\":\"ingress.k8s.aws/resource\",\"Value\":\"LoadBalancer\"}) ) )
+        | .ResourceArn '  && echo '\u001b[32m  Pruned security groups  \u001b[33m'  || echo '\u001b[33m  Did not find security groups to prune. \u001b[0m' 
+)
+
   ",
   exit_on_fail: false,
 )
